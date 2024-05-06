a3cd2e3191665f07438ecef52660f633cd2bf6be
Miguel Ibars <miguel.ibars@intelygenz.com>
2024-05-06 19:21:37 +0200

    Use compact constructor for reassigning record components

--- src/main/java/com/test/intelygenztest/domain/OrderedIntegers.java
+++ src/main/java/com/test/intelygenztest/domain/OrderedIntegers.java
@@ -3,20 +3,22 @@ package com.test.intelygenztest.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record OrderedIntegers(List<Integer> numbers)
-{
-    public OrderedIntegers(List<Integer> numbers) {
-        this.numbers = numbers.stream().sorted((a, b) -> {
-            if (Integer.bitCount(a) == Integer.bitCount(b)) {
-                // if both have the same number of bits
-                return a - b;    // smaller int value first
-            }
-            // otherwise biggest number of bits first
-            return Integer.bitCount(b) - Integer.bitCount(a);
-        }).collect(Collectors.toList());
-    }
+public record OrderedIntegers(List<Integer> numbers) {
 
-    public boolean isEmpty() {
-        return numbers.isEmpty();
-    }
+  public OrderedIntegers {
+    numbers = numbers.stream()
+        .sorted((a, b) -> {
+          if (Integer.bitCount(a) == Integer.bitCount(b)) {
+            // if both have the same number of bits
+            return a - b;    // smaller int value first
+          }
+          // otherwise biggest number of bits first
+          return Integer.bitCount(b) - Integer.bitCount(a);
+        })
+        .toList();
+  }
+
+  public boolean isEmpty() {
+    return numbers.isEmpty();
+  }
 }
