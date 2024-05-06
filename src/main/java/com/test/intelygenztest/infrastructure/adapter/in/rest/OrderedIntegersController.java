863643154998e99c65d86e2b53e272ccf2ace329
Miguel Ibars <miguel.ibars@intelygenz.com>
2024-05-06 17:38:22 +0200

    Set up Lombok for DI

--- src/main/java/com/test/intelygenztest/infrastructure/adapter/in/rest/OrderedIntegersController.java
+++ src/main/java/com/test/intelygenztest/infrastructure/adapter/in/rest/OrderedIntegersController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,17 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "OrderedIntegers", description = "Endpoints for ordered integers")
+@RequiredArgsConstructor
 public class OrderedIntegersController {
 
     private final GetOrderedIntegersByNumberOfOnesInBinaryValueUseCase getOrderedIntegersByNumberOfOnesInBinaryValueUseCase;
 
     private final OrderedIntegersMapper orderedIntegersMapper;
-
-
-    public OrderedIntegersController(GetOrderedIntegersByNumberOfOnesInBinaryValueUseCase getOrderedIntegersByNumberOfOnesInBinaryValueUseCase, OrderedIntegersMapper orderedIntegersMapper) {
-        this.getOrderedIntegersByNumberOfOnesInBinaryValueUseCase = getOrderedIntegersByNumberOfOnesInBinaryValueUseCase;
-        this.orderedIntegersMapper = orderedIntegersMapper;
-    }
 
     @GetMapping("/orderedIntegersByBinaryValue")
     @Operation(summary = "Get OrderedIntegers by number of 1s in its binary value")
