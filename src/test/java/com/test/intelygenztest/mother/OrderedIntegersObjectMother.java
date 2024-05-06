44f3bd1e6b245feddaf8ab01a0138a8a486e2770
Miguel Ibars <miguel.ibars@intelygenz.com>
2024-05-06 19:16:41 +0200

    Use factory method List.of()

--- src/test/java/com/test/intelygenztest/mother/OrderedIntegersObjectMother.java
+++ src/test/java/com/test/intelygenztest/mother/OrderedIntegersObjectMother.java
@@ -9,37 +9,25 @@ import java.util.List;
 
 public class OrderedIntegersObjectMother {
 
-    public static final String ORDERED_INTEGERS_RESOURCES = "/orderedIntegersByBinaryValue";
+  public static final String ORDERED_INTEGERS_RESOURCES = "/orderedIntegersByBinaryValue";
 
-    public static OrderedIntegers getOrderedIntegers() {
-        List<Integer> orderedIntegers = new ArrayList<>();
-        orderedIntegers.add(15);
-        orderedIntegers.add(7);
-        orderedIntegers.add(3);
-        orderedIntegers.add(5);
-        orderedIntegers.add(1);
-        return new OrderedIntegers(orderedIntegers);
-    }
+  public static OrderedIntegers getOrderedIntegers() {
+    return new OrderedIntegers(List.of(15, 7, 3, 5, 1));
+  }
 
-    public static OrderedNumberListDTO getNumberListDTO() {
-        List<Integer> orderedIntegers = new ArrayList<>();
-        orderedIntegers.add(15);
-        orderedIntegers.add(7);
-        orderedIntegers.add(3);
-        orderedIntegers.add(5);
-        orderedIntegers.add(1);
-        return new OrderedNumberListDTO(orderedIntegers);
-    }
+  public static OrderedNumberListDTO getNumberListDTO() {
+    return new OrderedNumberListDTO(List.of(15, 7, 3, 5, 1));
+  }
 
-    public static List<OrderedIntegersJPADTO> getOrderedIntegersJPADTO() {
-        List<OrderedIntegersJPADTO> orderedIntegersJPADTOS = new ArrayList<>();
-        orderedIntegersJPADTOS.add(new OrderedIntegersJPADTO(1L, 15));
-        orderedIntegersJPADTOS.add(new OrderedIntegersJPADTO(2L, 7));
-        orderedIntegersJPADTOS.add(new OrderedIntegersJPADTO(3L, 3));
-        orderedIntegersJPADTOS.add(new OrderedIntegersJPADTO(4L, 5));
-        orderedIntegersJPADTOS.add(new OrderedIntegersJPADTO(5L, 1));
-        return orderedIntegersJPADTOS;
-    }
+  public static List<OrderedIntegersJPADTO> getOrderedIntegersJPADTO() {
+    return List.of(
+        new OrderedIntegersJPADTO(1L, 15),
+        new OrderedIntegersJPADTO(2L, 7),
+        new OrderedIntegersJPADTO(3L, 3),
+        new OrderedIntegersJPADTO(4L, 5),
+        new OrderedIntegersJPADTO(5L, 1)
+    );
+  }
 
 
 }
